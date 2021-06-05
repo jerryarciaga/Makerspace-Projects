2d899eacb8a1d711a80bbccfd79de872fb669915
Jerry Arciaga <jerryarciaga11@gmail.com>
2021-06-05 16:59:24 -0600

    Code cleanup. TODO: Figure out Power Reduction to further reduce power consumption in AVR 2560

--- Learning Tablet/test.c
+++ Learning Tablet/test.c
@@ -13,21 +13,34 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
-ISR(PCINT2_vect) {
+ISR(PCINT2_vect) { // Add this ISR Function on top of setup() and loop()
     _delay_ms(5);
+    cli();
+    if(PINK & (1<<PINK1)) { // If off button is pressed
+        PCMSK2 &= ~(1 << PCINT17); // Disable Off Button Interrupt
+        sleep_enable();
+        sei();
+        sleep_cpu();
+        sleep_disable();
+    }
+    if(PINK & (1<<PINK0)) { // If on button is pressed
+        PCMSK2 |= (1 << PCINT17); // Enable Off Button Interrupt
+    }
+    sei();
 
     return;
 }
 
 
 int main() {
-
+    
+    /* Add this snippet of code into void setup() */
     // Set both ON and OFF buttons as inputs; set PCMSK
     DDRK &= ~((1 << DDK0) & (1 << DDK1));
 
     // Enable PCINT2, then enable interrupts PCINT16 and PCINT17
     PCICR |= (1 << PCIE2);
-    PCMSK2 |= (1 << PCINT16);
+    PCMSK2 |= (1 << PCINT16) | (1 << PCINT17);
 
     // Test function using built in LED (PB7); set as output
     DDRB |= (1 << DDB7);
@@ -35,6 +48,7 @@ int main() {
     sei();
     // Set sleep mode
     set_sleep_mode(2); // Refer to Table 11-2
+    /* End of void setup()*/
         
     // Simulate a microcontroller running a program (Blinking LED)
     while(1) {
@@ -42,16 +56,6 @@ int main() {
         _delay_ms(250);
         PORTB &= ~(1 << PB7);
         _delay_ms(250);
-
-        cli();
-        if(PINK & (1<<PINK1)) { // If off button is pressed
-            _delay_ms(10);
-            sleep_enable();
-            sei();
-            sleep_cpu();
-            sleep_disable();
-        }
-        sei();
     }
 
     return 0;
